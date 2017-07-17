bbd27b12c08f1fb232afcc7b759a8bf8520867a2
drknave <drknave@users.noreply.github.com>
2017-07-16 20:59:25 -0500

    Fleshed out the listing section with NEO data. Lots of style changes.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,73 @@
 $(document).ready(function() {
 
-	var asteroidData = fetchAsteroidData();
-	var nearEarthObjects = getNearEarthObjects(asteroidData);
-	createDomElements(nearEarthObjects);
+	setUpNavigationClickHandling();
+
+	var nearEarthObjectData = fetchNearEarthObjectData();
+	var nearEarthObjects = getNearEarthObjects(nearEarthObjectData);
+
+	generateListing(nearEarthObjects);
 });
 
-function fetchAsteroidData() {
+function setUpNavigationClickHandling() {
+
+	$("#listingLink").click(function() {
+		updateNavigationSelection("listingLink");
+	});
+
+	$("#chartLink").click(function() {
+		updateNavigationSelection("chartLink");
+	});
+
+	$("#aboutLink").click(function() {
+		updateNavigationSelection("aboutLink");
+	});
+}
+
+function updateNavigationSelection(selectedLink) {
+
+	var children = $("nav").children();
+
+	children.each(function() {
+		if(this.id === selectedLink) {
+			$(this).addClass('selected');
+		} else {
+			$(this).removeClass('selected');
+		}
+	});
+}
+
+function fetchNearEarthObjectData() {
 	// Just returning test data for now
 	return testData;
 }
 
-function getNearEarthObjects(asteroidData) {
+function getNearEarthObjects(nearEarthObjectData) {
 
 	var nearEarthObjects = [];
-	var dateGroupings = Object.keys(asteroidData.near_earth_objects);
+	var dateGroupings = Object.keys(nearEarthObjectData.near_earth_objects);
 
 	dateGroupings.forEach(function(grouping) {
-		Array.prototype.push.apply(nearEarthObjects, asteroidData.near_earth_objects[grouping]);
+		Array.prototype.push.apply(nearEarthObjects, nearEarthObjectData.near_earth_objects[grouping]);
 	});
 
 	return nearEarthObjects;
 }
 
-function createDomElements(nearEarthObjects) {
+function generateListing(nearEarthObjects) {
+
+	// Create an unordered list to contain entries
+	var list = $('<ul/>').appendTo('#listingArea');
 
 	nearEarthObjects.forEach(function(nearEarthObject) {
-		$('<div/>', {
+		list.append("<li>" + 
+			"<h2>" + nearEarthObject.name.replace(/[\(\)]/g, '') + "</h2>" +
+			"<p>Reference ID: <a href='" + nearEarthObject.nasa_jpl_url + "' target='_blank'>" + nearEarthObject.neo_reference_id + "</a></p>" +
+			"<p>Estimated Diameter: Between " + nearEarthObject.estimated_diameter.meters.estimated_diameter_min + " and " + nearEarthObject.estimated_diameter.meters.estimated_diameter_max + " meters</p>" +
+			"<p>Missed the earth by " + nearEarthObject.close_approach_data[0].miss_distance.kilometers + " kilometers today</p>" +
+			"<p>Orbiting " + nearEarthObject.close_approach_data[0].orbiting_body + " at a rate of " + nearEarthObject.close_approach_data[0].relative_velocity.kilometers_per_second + " kilometers per second</p>" +
+		"</li>");
+		/*$('<div/>', {
 			text: nearEarthObject.name
-		}).appendTo('#attachPoint');
+		}).appendTo('#listingArea');*/
 	});
 }
